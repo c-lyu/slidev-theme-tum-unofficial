455ce7cfca7d2ff85d1cc416dc832ea632c3b6a6
c-lyu <cheng.lyu@outlook.com>
2024-06-04 15:41:03 +0200

    Auto show section title

--- layouts/section.vue
+++ layouts/section.vue
@@ -1,26 +1,48 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { handleBackground } from '../layoutHelper'
+import { computed } from "vue";
+import type { SlideInfoBase } from "@slidev/types";
 
-const props = defineProps({
-  footer: {
-    default: false,
-  },
-})
+const sections = computed(() => {
+  const result: [string, number[]][] = [];
+  let pages: number[] = [];
+  let title = "";
+  console.log(typeof($slidev.nav.slides))
+  for(let i = 1; i < $slidev.nav.slides.length; i++) {
+    const slide = $slidev.nav.slides[i];
+    const section = (slide.meta?.slide as SlideInfoBase)?.frontmatter?.section;
+    if(section && section != title) {
+      if(pages.length > 0) result.push([title, pages]);
+      pages = [];
+      title = section;
+    }
+    pages.push(i);
+  }
+  result.push([title, pages]);
+  return result;
+});
 
-// const style = computed(() => handleBackground(props.background, true))
+const currentSection = computed(() => {
+  const currentPage = $slidev.nav.currentPage - 1; // Adjust for 0-based index
+  for (const [title, pages] of sections.value) {
+    if (pages.includes(currentPage)) {
+      return title;
+    }
+  }
+  return "";
+});
 </script>
 
 <style scoped>
-
 .section-back {
   background-color: #005cb1;
 }
 </style>
 
 <template>
+  <TumLogo white=true />
   <div class="slidev-layout w-full h-full grid section-back">
     <div class="my-auto text-white">
+      <h1>{{ currentSection }}</h1>
       <slot />
     </div>
   </div>
