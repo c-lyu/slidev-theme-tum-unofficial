798587d7137d0643025d5add85fb391430e84be6
c-lyu <cheng.lyu@outlook.com>
2024-06-04 23:36:28 +0200

    Add image component

---
+++ components/Image.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+  },
+  figureClass: {
+    type: String,
+  },
+  class: {
+    type: String,
+  }
+})
+</script>
+
+<template>
+  <figure :class="figureClass">
+    <img :src="src" :alt="alt" :class="class" />
+    <figcaption v-if="alt">{{ alt }}</figcaption>
+  </figure>
+</template>
