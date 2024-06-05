5f769dea69baeaf0652e28af782c42aa484d27e0
c-lyu <cheng.lyu@outlook.com>
2024-06-06 00:43:01 +0200

    Update style and optimize

--- components/Card.vue
+++ components/Card.vue
@@ -13,15 +13,31 @@ const props = defineProps({
   },
 })
 
+interface ColorPresets {
+  [key: string]: string;
+}
+
+const colorPresets: ColorPresets = {
+  'tumblue': 'text-tumblue-700',
+  'red': 'text-red-700',
+  'green': 'text-green-700',
+  'sky': 'text-sky-700',
+  'blue': 'text-blue-700',
+  'yellow': 'text-yellow-700',
+  'purple': 'text-purple-700',
+  'pink': 'text-pink-700',
+  'orange': 'text-orange-700',
+};
+
 const titleColor = computed(() => {
-  return `text-${props.color}-700`;
+  return colorPresets[props.color];
 });
 </script>
 
 <template>
     <div class="card-container">
         <div class="card-container-inner">
-            <h2 :class="[titleColor]">{{ props.title }}</h2>
+            <h2 :class="titleColor">{{ props.title }}</h2>
         </div>
         <div class="card-content">
             <slot />
