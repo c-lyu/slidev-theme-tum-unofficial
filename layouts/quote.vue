ab48810e4d00c12f53cc0b41d55fc12f53dd7deb
c-lyu <cheng.lyu@outlook.com>
2025-03-28 20:41:59 +0100

    Enhance quote layout

--- layouts/quote.vue
+++ layouts/quote.vue
@@ -1,7 +1,11 @@
 <template>
+  <TumLogo />
   <div class="slidev-layout quote">
     <div class="main-content">
-      <slot />
+      <div class="h-full w-3/4 flex flex-col justify-center items-center">
+        <slot />
+      </div>
     </div>
   </div>
+  <Footer v-if="$slidev.configs.footer ?? true" />
 </template>
